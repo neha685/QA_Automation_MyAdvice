2d8751df9bb7ad769621d6e5ae9bbd81b2764716
unknown <neha.patil@myadvice.com>
2024-11-21 17:42:38 +0530

    2 test cases added

--- src/main/java/Utility/UtilClass.java
+++ src/main/java/Utility/UtilClass.java
@@ -1,31 +1,34 @@
 package Utility;
+
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import BasePackage.BaseClass;
 
-
 public class UtilClass extends BaseClass {
-	public static long page_load_timeOut=20;
-	public static long implicit_wait=10;
+	public static long page_load_timeOut = 20;
+	public static long implicit_wait = 10;
 	public WebDriverWait wait;
-	
+
 	public UtilClass() {
 		super();
 	}
-	
+
 	public static void scrollUsingJavaScript() {
-		JavascriptExecutor js=(JavascriptExecutor)driver;
+		JavascriptExecutor js = (JavascriptExecutor) driver;
 		js.executeScript("window.scrollBy(0,500);");
 	}
-	
+
 	public static void switchWindow(WebDriver driver, WebElement ele, String exp) {
 		String parentWindow = driver.getWindowHandle();
 		String heading = null;
@@ -41,14 +44,42 @@ public class UtilClass extends BaseClass {
 		} else
 			System.out.println("Child window is not present");
 	}
-	
+
 	public static List<String> listSelectTagOptions(WebElement ele) {
 		List<String> optionTexts = new ArrayList<>();
-		Select drpdwn=new Select(ele);
-		List<WebElement> drpdwnOptions=drpdwn.getOptions();
-		for(WebElement ele1:drpdwnOptions) {
+		Select drpdwn = new Select(ele);
+		List<WebElement> drpdwnOptions = drpdwn.getOptions();
+		for (WebElement ele1 : drpdwnOptions) {
 			optionTexts.add(ele1.getText());
 		}
-		return optionTexts;	
-}
+		return optionTexts;
+	}
+
+	public static void clickUsingJavaScriptExecutor(WebElement ele) {
+		JavascriptExecutor js=(JavascriptExecutor)driver;
+		js.executeScript("arguments[0].click();", ele);
+	}
+	
+	public static void waitImplicitly() {
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(implicit_wait));
+	}
+	
+	public static void switchTabs() {
+		try {
+			String originalWindow=driver.getWindowHandle();
+			
+			Set<String> windowhandles=driver.getWindowHandles();
+			while(windowhandles.size()==1) {
+				windowhandles=driver.getWindowHandles();
+			}
+			for(String windowhandle:windowhandles) {
+				if(!windowhandle.equals(originalWindow)) {
+					driver.switchTo().window(windowhandle);
+					break;
+				}
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
--- src/test/java/PageTests/LeadManagementTest.java
+++ src/test/java/PageTests/LeadManagementTest.java
@@ -21,7 +21,7 @@ public class LeadManagementTest extends BaseClass{
 		leadPage=ln.loginToPC(prop.getProperty("emailid"),prop.getProperty("password"));
 		}
 	
-	@Test(priority=1, enabled=false, description="verification of lead management dropdown", groups= {"Regression"})
+	@Test(priority=1, enabled=true, description="verification of lead management dropdown", groups= {"Regression"})
 	public void verifyLeadManagementSubMenus() {
 		Assert.assertTrue(leadPage.verificationOfSubMenusList());
 	}
@@ -34,7 +34,7 @@ public class LeadManagementTest extends BaseClass{
 		Assert.assertTrue(result);	
 	}
 	
-	@Test(priority=3, enabled=false, description="Verification of search field")
+	@Test(priority=3, enabled=true, description="Verification of search field")
 	public void verifySearchField() {
 		Assert.assertTrue(leadPage.verifySearchField());
 	}
@@ -43,20 +43,29 @@ public class LeadManagementTest extends BaseClass{
 	public void verifyAdvancedFilter() {
 		Assert.assertTrue(leadPage.verifyAdvancedFilter());
 	}
-	@Test(priority=4, enabled=false, description="verification of add note feature", groups= {"smoke"})
+	@Test(priority=5, enabled=true, description="verification of add note feature", groups= {"smoke"})
 	public void verifyAddNoteFeature() {
 		Assert.assertTrue(leadPage.addNoteFeature());
 	}
 	
-	@Test(priority=5, enabled=true, description="verification of edit lead")
+	@Test(priority=6, enabled=true, description="verification of edit lead")
 	public void verifyEditLeadFeature() {
 		Assert.assertTrue(leadPage.verifyEditLeadFeature(prop.getProperty("neha_edit@gmail.com")));
 	}
 	
 	//to be executed at the last so that large data wont be created
-	@Test(priority=6, enabled=false, description="verification of lead deletion")
+	@Test(priority=7, enabled=true, description="verification of lead deletion")
 	public void deleteLead() {
 		Assert.assertTrue(leadPage.verifyLeadDeletion());
 	}
 	
+	@Test(priority=8, enabled=true, description="verification of people submenu")
+	public void verifyPeopleSubmenu() {
+		Assert.assertTrue(leadPage.verificationOfPeopleSubmenu(prop.getProperty("people_url")));
+	} 
+	
+	@Test(priority=9, enabled=true, description="verification of view CTA")
+	public void verifyViewCTA() {
+		Assert.assertTrue(leadPage.verifyViewCTAinPeople(prop.getProperty("people_details")));
+	}
 }
